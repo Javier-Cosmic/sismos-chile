12eea7346c517b597e11cc52a21968c19634403d
javier_contreras <javiercontreras.tome@gmail.com>
2020-05-10 19:38:17 -0400

    Updates

---
+++ precache-manifest.cc362fbfdf5b884f4fb7833e807eaf46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50fba6da5556527a6570e1e2dd1206f2",
+    "url": "/sismos-chile/index.html"
+  },
+  {
+    "revision": "19f45268228d1493bafb",
+    "url": "/sismos-chile/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d00b9d7bc0a0161d588d",
+    "url": "/sismos-chile/static/js/2.7d38955a.chunk.js"
+  },
+  {
+    "revision": "3a08a7c8f8e6231522abe29a929e6912",
+    "url": "/sismos-chile/static/js/2.7d38955a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19f45268228d1493bafb",
+    "url": "/sismos-chile/static/js/main.00f85d6d.chunk.js"
+  },
+  {
+    "revision": "6e854e0ec2e42135653d",
+    "url": "/sismos-chile/static/js/runtime-main.10cb7647.js"
+  }
+]);
